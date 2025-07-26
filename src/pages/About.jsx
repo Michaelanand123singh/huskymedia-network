cede269dfec076c07f022dd4a84bff14f02ef7ff
Anand Singh <111451218+Michaelanand123singh@users.noreply.github.com>
2025-07-26 22:22:30 +0530

    trigger vercel deployment

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,5 @@
 import AboutHero from '../components/about/AboutHero';
 import CompanyStory from '../components/about/CompanyStory';
-import TeamSection from '../components/about/TeamSection';
 import BenefitsSection from '../components/about/BenifitsSection';
 import StatsSection from '../components/about/StatsSection';
 
@@ -9,7 +8,6 @@ const About = () => {
     <div className="min-h-screen">
       <AboutHero />
       <CompanyStory />
-      <TeamSection />
       <BenefitsSection />
       <StatsSection />
     </div>
